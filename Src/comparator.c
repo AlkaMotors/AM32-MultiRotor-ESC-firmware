8b3106613015de3bcb89c10ca7e5896bbda18115
DusKing1 <1483569698@qq.com>
2020-10-31 20:30:23 +0800

    Comparator code cleanup

--- Src/comparator.c
+++ Src/comparator.c
@@ -8,63 +8,62 @@
 #include "comparator.h"
 #include "targets.h"
 
+#ifdef use_A0_B4_C5_comp_order
+#define PHASE_A_COMP COMP_PA0
+#define PHASE_B_COMP COMP_PA4
+#define PHASE_C_COMP COMP_PA5
+#endif
+
+#ifdef use_A4_B5_C0_comp_order
+#define PHASE_A_COMP COMP_PA4
+#define PHASE_B_COMP COMP_PA5
+#define PHASE_C_COMP COMP_PA0
+#endif
+
+#ifdef use_A5_B4_C0_comp_order
+#define PHASE_A_COMP COMP_PA5
+#define PHASE_B_COMP COMP_PA4
+#define PHASE_C_COMP COMP_PA0
+#endif
 
 void maskPhaseInterrupts(){
 	EXTI->IMR &= (0 << 21);
 }
 
-void changeCompInput() {
-//	TIM3->CNT = 0;
-//	HAL_COMP_Stop_IT(&hcomp1);            // done in comparator interrupt routine
+void changeCompInput()
+{
+	//	TIM3->CNT = 0;
+	//	HAL_COMP_Stop_IT(&hcomp1);            // done in comparator interrupt routine
 
-	if (step == 1 || step == 4) {   // c floating
-	//	hcomp1.Init.InvertingInput = COMP_INVERTINGINPUT_IO1;
-#ifdef use_A0_B4_C5_comp_order
-		COMP->CSR = 0b1010001;        // pa5
-#else
-		COMP->CSR = 0b1100001;         // pa0
-#endif
+	if (step == 1 || step == 4)
+	{   // c floating
+		//	hcomp1.Init.InvertingInput = COMP_INVERTINGINPUT_IO1;
+		COMP->CSR = PHASE_C_COMP;
 	}
 
-	if (step == 2 || step == 5) {     // a floating
-#ifdef use_A4_B5_C0_comp_order
-		//COMP->CSR = 0x40;
-	//	hcomp1.Init.InvertingInput = COMP_INVERTINGINPUT_DAC1;
-		COMP->CSR = 0b1000001;                        /// if f051k6  step 2 , 5 is dac 1 ( swap comp input)
-#endif
-
-#ifdef use_A5_B4_C0_comp_order
-	//hcomp1.Init.InvertingInput = COMP_INVERTINGINPUT_DAC2;      // PA5
-		COMP->CSR = 0b1010001;
-#endif
-#ifdef use_A0_B4_C5_comp_order
-	                                                              // PA0
-		COMP->CSR = 0b1100001;
-#endif
+	if (step == 2 || step == 5) 
+	{   // a floating
+		//	hcomp1.Init.InvertingInput = COMP_INVERTINGINPUT_DAC1;
+		COMP->CSR = PHASE_A_COMP;
 	}
 
-	if (step == 3 || step == 6) {      // b floating
-#ifdef use_A4_B5_C0_comp_order
-		//COMP->CSR = 0x50
-	//	hcomp1.Init.InvertingInput = COMP_INVERTINGINPUT_DAC2;
-		COMP->CSR = 0b1010001;
-#endif
-#if defined (use_A5_B4_C0_comp_order) || defined (use_A0_B4_C5_comp_order)
-	//	hcomp1.Init.InvertingInput = COMP_INVERTINGINPUT_DAC1;       // PA4
-		COMP->CSR = 0b1000001;
-#endif
+	if (step == 3 || step == 6) 
+	{   // b floating
+		//	hcomp1.Init.InvertingInput = COMP_INVERTINGINPUT_DAC2;
+		COMP->CSR = PHASE_B_COMP;
 	}
-	if (rising){
 
+	if (rising)
+	{
 		EXTI->RTSR = 0x0;
-	EXTI->FTSR = 0x200000;
+		EXTI->FTSR = 0x200000;
 
-	//	hcomp1.Init.TriggerMode = COMP_TRIGGERMODE_IT_FALLING;   // polarity of comp output reversed
-	}else{                          // falling bemf
+		//	hcomp1.Init.TriggerMode = COMP_TRIGGERMODE_IT_FALLING;   // polarity of comp output reversed
+		}else{                          // falling bemf
 
-	EXTI->FTSR = 0x0;
-	EXTI->RTSR = 0x200000;
-	//	hcomp1.Init.TriggerMode = COMP_TRIGGERMODE_IT_RISING;
+		EXTI->FTSR = 0x0;
+		EXTI->RTSR = 0x200000;
+		//	hcomp1.Init.TriggerMode = COMP_TRIGGERMODE_IT_RISING;
 	}
 
 
