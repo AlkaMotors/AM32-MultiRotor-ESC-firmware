b8ca903475575a9ff19084c242382a23aee4724f
BX32_Brushless <37931458+conuthead@users.noreply.github.com>
2021-07-06 19:20:28 -0300

    incease range for servo detection

--- Mcu/f051/Src/IO.c
+++ Mcu/f051/Src/IO.c
@@ -201,14 +201,14 @@ void detectInput(){
 //	if ((smallestnumber > 2000 )&&(smallestnumber < 3000)){
 //		oneshot42 = 1;
 //	}
-		if (smallestnumber > 1000){
+		if (smallestnumber > 300 && smallestnumber < 20000){
 			servoPwm = 1;
 			ic_timer_prescaler=47;
 			armed_count_threshold = 35;
 			buffersize = 2;
 		}
 
-	if (smallestnumber == 0){
+	if (smallestnumber == 0 || smallestnumber == 20000){
 		inputSet = 0;
 	}else{
 
@@ -218,125 +218,7 @@ void detectInput(){
 }
 
 
-//void computeMSInput(){
 
-	//int lastnumber = dma_buffer[0];
-	//for ( int j = 1 ; j < 2; j++){
-
-		//if(((dma_buffer[j] - lastnumber) < 1500) && ((dma_buffer[j] - lastnumber) > 0)){ // blank space
-
-			//newinput = map((dma_buffer[j] - lastnumber),243,1200, 0, 2000);
-			//break;
-		//}
-		//lastnumber = dma_buffer[j];
-	//}
-//}
-
-
-//void computeServoInput(){
-
-	//int lastnumber = dma_buffer[0];
-	//for ( int j = 1 ; j < 2; j++){
-
-		//if(((dma_buffer[j] - lastnumber) >900 ) && ((dma_buffer[j] - lastnumber) < 2150)){ // blank space
-
-			//if(bi_direction){
-				//if(dma_buffer[j] - lastnumber <= servo_neutral){
-				//servorawinput = map((dma_buffer[j] - lastnumber), servo_low_threshold, servo_neutral, 0, 1000);
-				//}else{
-				//servorawinput = map((dma_buffer[j] - lastnumber), servo_neutral+1, servo_high_threshold, 1001, 2000);
-				//}
-			//}else{
-			//servorawinput = map((dma_buffer[j] - lastnumber), servo_low_threshold, servo_high_threshold, 0, 2000);
-			//}
-			//break;
-		//}
-		//lastnumber = dma_buffer[j];
-	//}
-	//if (servorawinput - newinput > max_servo_deviation){
-		//newinput += max_servo_deviation;
-	//}else if(newinput - servorawinput > max_servo_deviation){
-		//newinput -= max_servo_deviation;
-	//}else{
-		//newinput = servorawinput;
-	//}
-
-//}
-
-//void transfercomplete(){
-	//if(armed && dshot_telemetry){
-	    //if(out_put){
-
-
-	  	//receiveDshotDma();
-	   	//return;
-	    //}else{
-
-			//sendDshotDma();
-			//make_dshot_package();
-			//computeDshotDMA();
-	    //return;
-	    //}
-	//}
-
-	  //if (inputSet == 0){
-	 	 //detectInput();
-	 	//receiveDshotDma();
-	 //return;
-	  //}
-
-	//if (inputSet == 1){
-	//if(!armed){
-		//signaltimeout = 0;
-		//if (adjusted_input < 0){
-			//adjusted_input = 0;
-			  					//}
-		 		 //if (adjusted_input == 0){                       // note this in input..not newinput so it will be adjusted be main loop
-		 		 			//zero_input_count++;
-		 		 		//}else{
-		 		 			//zero_input_count = 0;
-		 		 		//}
-		//}
-
-//if(dshot_telemetry){
-    //if(out_put){
-////    	TIM17->CNT = 0;
-    	//make_dshot_package();          // this takes around 10us !!
-  	//computeDshotDMA();             //this is slow too..
-  	//receiveDshotDma();             //holy smokes.. reverse the line and set up dma again
-   	//return;
-    //}else{
-		//sendDshotDma();
-    //return;
-    //}
-//}else{
-
-		//if (dshot == 1){
-			//computeDshotDMA();
-			//if(send_telemetry){
-////			  makeTelemPackage(degrees_celsius,
-////					  ADC_raw_volts,
-////			  					  1000,
-////			  					  200,
-////			  					  k_erpm);
-////			  send_telem_DMA();
-			//}
-			//receiveDshotDma();
-		//}
-
-
-		//if  (servoPwm == 1){
-			//computeServoInput();
-		////	IC_TIMER_REGISTER->CNT = 0;
-			//signaltimeout = 0;
-			//LL_TIM_IC_SetPolarity(IC_TIMER_REGISTER, IC_TIMER_CHANNEL, LL_TIM_IC_POLARITY_RISING); // setup rising pin trigger.
-     		//receiveDshotDma();
-     	    //LL_DMA_EnableIT_HT(DMA1, INPUT_DMA_CHANNEL);
-		//}
-
-	//}
-	//}
-//}
 
 
 
