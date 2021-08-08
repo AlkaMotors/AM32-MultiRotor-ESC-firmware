10b3462c7012f6feef0a9ac87f665b2ea681836b
Voodoobrew101 <Voodoobrew@gmail.com>
2021-08-08 00:05:28 -0700

    Correct Indentation for main.c

--- Src/main.c
+++ Src/main.c
@@ -4,110 +4,111 @@
 //=============================== Changelog =================================
 //===========================================================================
 /*
- * 1.54 Changelog;
- * --Added firmware name to targets and firmware version to main
- * --added two more dshot to beacons 1-3 currently working
- * --added KV option to firmware, low rpm power protection is based on KV
- * --start power now controls minimum idle power as well as startup strength.
- * --change default timing to 22.5
- * --Lowered default minimum idle setting to 1.5 percent duty cycle, slider range from 1-2.
- * --Added dshot commands to save settings and reset ESC.
- *
- *1.56 Changelog.
- * -- added check to stall protection to wait until after 40 zero crosses to fix high startup throttle hiccup.
- * -- added TIMER 1 update interrupt and PWM changes are done once per pwm period
- * -- reduce commutation interval averaging length
- * -- reduce false positive filter level to 2 and eliminate threshold where filter is stopped.
- * -- disable interrupt before sounds
- * -- disable TIM1 interrupt during stepper sinusoidal mode
- * -- add 28us delay for dshot300
- * -- report 0 rpm until the first 10 successful steps.
- * -- move serial ADC telemetry calculations and desync check to 10Khz interrupt.
- *
- * 1.57
- * -- remove spurious commutations and rpm data at startup by polling for longer interval on startup
- *
- * 1.58
- * -- move signal timeout to 10khz routine and set armed timeout to one quarter second 2500 / 10000
- * 1.59
- * -- moved comp order definitions to target.h
- * -- fixed update version number if older than new version
- * -- cleanup, moved all input and output to IO.c
- * -- moved comparator functions to comparator.c
- * -- removed ALOT of useless variables
- * -- added siskin target
- * -- moved pwm changes to 10khz routine
- * -- moved basic functions to functions.c
- * -- moved peripherals setup to periherals.c
- * -- added crawler mode settings
- *
- * 1.60
- * -- added sine mode hysteresis
- * -- increased power in stall protection and lowered start rpm for crawlers
- * -- removed onehot125 from crawler mode
- * -- reduced maximum startup power from 400 to 350
- * -- change minimum duty cycle to DEAD_TIME
- * -- version and name moved to permanent spot in FLASH memory, thanks mikeller
- *
- * 1.61
- * -- moved duty cycle calculation to 10khz and added max change option.
- * -- decreased maximum interval change to 25%
- * -- reduce wait time on fast acceleration (fast_accel)
- * -- added check in interrupt for early zero cross
- *
- * 1.62
- * --moved control to 10khz loop
- * --changed condition for low rpm filter for duty cycle from || to &&
- * --introduced max deceleration and set it to 20ms to go from 100 to 0
- * --added configurable servo throttle ranges
- *
- *
- *1.63
- *-- increase time for zero cross error detection below 250us commutation interval
- *-- increase max change a low rpm x10
- *-- set low limit of throttle ramp to a lower point and increase upper range
- *-- change desync event from full restart to just lower throttle.
-
- *1.64
- * --added startup check for continuous high signal, reboot to enter bootloader.
- *-- added brake on stop from eeprom
- *-- added stall protection from eeprom
- *-- added motor pole divider for sinusoidal and low rpm power protection
- *-- fixed dshot commands, added confirmation beeps and removed blocking behavior
- *--
- *1.65
- *-- Added 32 millisecond telemetry output
- *-- added low voltage cutoff , divider value and cutoff voltage needs to be added to eeprom
- *-- added beep to indicate cell count if low voltage active
- *-- added current reading on pa3 , conversion factor needs to be added to eeprom
- *-- fixed servo input capture to only read positive pulse to handle higher refresh rates.
- *-- disabled oneshot 125.
- *-- extended servo range to match full output range of receivers
- *-- added RC CAR style reverse, proportional brake on first reverse , double tap to change direction
- *-- added brushed motor control mode
- *-- added settings to EEPROM version 1
- *-- add gimbal control option.
- *--
- *1.66
- *-- move idwg init to after input tune
- *-- remove reset after save command -- dshot
- *-- added wraith32 target
- *-- added average pulse check for signal detection
- *--
- *1.67
- *-- Rework file structure for multiple MCU support
- *-- Add g071 mcu
- *--
- *1.68
- *--increased allowed average pulse length to avoid double startup
- *1.69
- *--removed line re-enabling comparator after disabling.
- *1.70 fix dshot for Kiss FC
- *1.71 fix dshot for Ardupilot / Px4 FC
- *1.72 Fix telemetry output and add 1 second arming.
- *1.73 Fix false arming if no signal. Remove low rpm throttle protection below 300kv
- *1.74 Add Sine Mode range and drake brake strength adjustment
- */
+* 1.54 Changelog;
+* --Added firmware name to targets and firmware version to main
+* --added two more dshot to beacons 1-3 currently working
+* --added KV option to firmware, low rpm power protection is based on KV
+* --start power now controls minimum idle power as well as startup strength.
+* --change default timing to 22.5
+* --Lowered default minimum idle setting to 1.5 percent duty cycle, slider range from 1-2.
+* --Added dshot commands to save settings and reset ESC.
+*
+*1.56 Changelog.
+* -- added check to stall protection to wait until after 40 zero crosses to fix high startup throttle hiccup.
+* -- added TIMER 1 update interrupt and PWM changes are done once per pwm period
+* -- reduce commutation interval averaging length
+* -- reduce false positive filter level to 2 and eliminate threshold where filter is stopped.
+* -- disable interrupt before sounds
+* -- disable TIM1 interrupt during stepper sinusoidal mode
+* -- add 28us delay for dshot300
+* -- report 0 rpm until the first 10 successful steps.
+* -- move serial ADC telemetry calculations and desync check to 10Khz interrupt.
+*
+* 1.57
+* -- remove spurious commutations and rpm data at startup by polling for longer interval on startup
+*
+* 1.58
+* -- move signal timeout to 10khz routine and set armed timeout to one quarter second 2500 / 10000
+*
+* 1.59
+* -- moved comp order definitions to target.h
+* -- fixed update version number if older than new version
+* -- cleanup, moved all input and output to IO.c
+* -- moved comparator functions to comparator.c
+* -- removed ALOT of useless variables
+* -- added siskin target
+* -- moved pwm changes to 10khz routine
+* -- moved basic functions to functions.c
+* -- moved peripherals setup to periherals.c
+* -- added crawler mode settings
+*
+* 1.60
+* -- added sine mode hysteresis
+* -- increased power in stall protection and lowered start rpm for crawlers
+* -- removed onehot125 from crawler mode
+* -- reduced maximum startup power from 400 to 350
+* -- change minimum duty cycle to DEAD_TIME
+* -- version and name moved to permanent spot in FLASH memory, thanks mikeller
+*
+* 1.61
+* -- moved duty cycle calculation to 10khz and added max change option.
+* -- decreased maximum interval change to 25%
+* -- reduce wait time on fast acceleration (fast_accel)
+* -- added check in interrupt for early zero cross
+*
+* 1.62
+* -- moved control to 10khz loop
+* -- changed condition for low rpm filter for duty cycle from || to &&
+* -- introduced max deceleration and set it to 20ms to go from 100 to 0
+* -- added configurable servo throttle ranges
+*
+*1.63
+* -- increase time for zero cross error detection below 250us commutation interval
+* -- increase max change a low rpm x10
+* -- set low limit of throttle ramp to a lower point and increase upper range
+* -- change desync event from full restart to just lower throttle.
+*
+*1.64
+* -- added startup check for continuous high signal, reboot to enter bootloader.
+* -- added brake on stop from eeprom
+* -- added stall protection from eeprom
+* -- added motor pole divider for sinusoidal and low rpm power protection
+* -- fixed dshot commands, added confirmation beeps and removed blocking behavior
+*
+*1.65
+* -- Added 32 millisecond telemetry output
+* -- added low voltage cutoff , divider value and cutoff voltage needs to be added to eeprom
+* -- added beep to indicate cell count if low voltage active
+* -- added current reading on pa3 , conversion factor needs to be added to eeprom
+* -- fixed servo input capture to only read positive pulse to handle higher refresh rates.
+* -- disabled oneshot 125.
+* -- extended servo range to match full output range of receivers
+* -- added RC CAR style reverse, proportional brake on first reverse , double tap to change direction
+* -- added brushed motor control mode
+* -- added settings to EEPROM version 1
+* -- add gimbal control option.
+*
+*1.66
+* -- move idwg init to after input tune
+* -- remove reset after save command -- dshot
+* -- added wraith32 target
+* -- added average pulse check for signal detection
+*
+*1.67
+* -- Rework file structure for multiple MCU support
+* -- Add g071 mcu
+*
+*1.68
+* -- increased allowed average pulse length to avoid double startup
+*
+*1.69
+* -- removed line re-enabling comparator after disabling.
+*1.70 fix dshot for Kiss FC
+*1.71 fix dshot for Ardupilot / Px4 FC
+*1.72 Fix telemetry output and add 1 second arming.
+*1.73 Fix false arming if no signal. Remove low rpm throttle protection below 300kv
+*1.74 Add Sine Mode range and drake brake strength adjustment
+*/
 #include <stdint.h>
 #include "main.h"
 #include "targets.h"
@@ -326,50 +327,50 @@ char rising = 1;
 
 ////Sine Wave PWM ///////////////////
 const int pwmSin[] = {180,183,186,189,193,196,199,202,
-		205,208,211,214,217,220,224,227,
-		230,233,236,239,242,245,247,250,
-		253,256,259,262,265,267,270,273,
-		275,278,281,283,286,288,291,293,
-		296,298,300,303,305,307,309,312,
-		314,316,318,320,322,324,326,327,
-		329,331,333,334,336,337,339,340,
-		342,343,344,346,347,348,349,350,
-		351,352,353,354,355,355,356,357,
-		357,358,358,359,359,359,360,360,
-		360,360,360,360,360,360,360,359,
-		359,359,358,358,357,357,356,355,
-		355,354,353,352,351,350,349,348,
-		347,346,344,343,342,340,339,337,
-		336,334,333,331,329,327,326,324,
-		322,320,318,316,314,312,309,307,
-		305,303,300,298,296,293,291,288,
-		286,283,281,278,275,273,270,267,
-		265,262,259,256,253,250,247,245,
-		242,239,236,233,230,227,224,220,
-		217,214,211,208,205,202,199,196,
-		193,189,186,183,180,177,174,171,
-		167,164,161,158,155,152,149,146,
-		143,140,136,133,130,127,124,121,
-		118,115,113,110,107,104,101,98,
-		95,93,90,87,85,82,79,77,
-		74,72,69,67,64,62,60,57,
-		55,53,51,48,46,44,42,40,
-		38,36,34,33,31,29,27,26,
-		24,23,21,20,18,17,16,14,
-		13,12,11,10,9,8,7,6,
-		5,5,4,3,3,2,2,1,
-		1,1,0,0,0,0,0,0,
-		0,0,0,1,1,1,2,2,
-		3,3,4,5,5,6,7,8,
-		9,10,11,12,13,14,16,17,
-		18,20,21,23,24,26,27,29,
-		31,33,34,36,38,40,42,44,
-		46,48,51,53,55,57,60,62,
-		64,67,69,72,74,77,79,82,
-		85,87,90,93,95,98,101,104,
-		107,110,113,115,118,121,124,127,
-		130,133,136,140,143,146,149,152,
-		155,158,161,164,167,171,174,177};
+	205,208,211,214,217,220,224,227,
+	230,233,236,239,242,245,247,250,
+	253,256,259,262,265,267,270,273,
+	275,278,281,283,286,288,291,293,
+	296,298,300,303,305,307,309,312,
+	314,316,318,320,322,324,326,327,
+	329,331,333,334,336,337,339,340,
+	342,343,344,346,347,348,349,350,
+	351,352,353,354,355,355,356,357,
+	357,358,358,359,359,359,360,360,
+	360,360,360,360,360,360,360,359,
+	359,359,358,358,357,357,356,355,
+	355,354,353,352,351,350,349,348,
+	347,346,344,343,342,340,339,337,
+	336,334,333,331,329,327,326,324,
+	322,320,318,316,314,312,309,307,
+	305,303,300,298,296,293,291,288,
+	286,283,281,278,275,273,270,267,
+	265,262,259,256,253,250,247,245,
+	242,239,236,233,230,227,224,220,
+	217,214,211,208,205,202,199,196,
+	193,189,186,183,180,177,174,171,
+	167,164,161,158,155,152,149,146,
+	143,140,136,133,130,127,124,121,
+	118,115,113,110,107,104,101,98,
+	95,93,90,87,85,82,79,77,
+	74,72,69,67,64,62,60,57,
+	55,53,51,48,46,44,42,40,
+	38,36,34,33,31,29,27,26,
+	24,23,21,20,18,17,16,14,
+	13,12,11,10,9,8,7,6,
+	5,5,4,3,3,2,2,1,
+	1,1,0,0,0,0,0,0,
+	0,0,0,1,1,1,2,2,
+	3,3,4,5,5,6,7,8,
+	9,10,11,12,13,14,16,17,
+	18,20,21,23,24,26,27,29,
+	31,33,34,36,38,40,42,44,
+	46,48,51,53,55,57,60,62,
+	64,67,69,72,74,77,79,82,
+	85,87,90,93,95,98,101,104,
+	107,110,113,115,118,121,124,127,
+	130,133,136,140,143,146,149,152,
+	155,158,161,164,167,171,174,177};
 
 
 //int sin_divider = 2;
@@ -1001,10 +1002,10 @@ void tenKhzRoutine(){
 	if(send_telemetry){
 #ifdef	USE_SERIAL_TELEMETRY
 		makeTelemPackage(degrees_celsius,
-				battery_voltage,
-				actual_current,
-				(uint16_t)consumed_current/10,
-				e_rpm);
+		battery_voltage,
+		actual_current,
+		(uint16_t)consumed_current/10,
+		e_rpm);
 		send_telem_DMA();
 		send_telemetry = 0;
 #endif
@@ -1242,7 +1243,7 @@ int main(void)
 	}
 
 	if(BRUSHED_MODE){bi_direction = 1;
-	commutation_interval = 5000;}
+		commutation_interval = 5000;}
 
 	if(BRUSHED_MODE){
 		playBrushedStartupTune();
@@ -1312,11 +1313,11 @@ int main(void)
 			}
 			adc_counter = 0;
 #ifdef USE_ADC_INPUT
-if(ADC_raw_input < 10){
-	zero_input_count++;
-}else{
-	zero_input_count=0;
-}
+			if(ADC_raw_input < 10){
+				zero_input_count++;
+			}else{
+				zero_input_count=0;
+			}
 #endif
 		}
 
@@ -1700,17 +1701,17 @@ void Error_Handler(void)
 
 #ifdef  USE_FULL_ASSERT
 /**
- * @brief  Reports the name of the source file and the source line number
- *         where the assert_param error has occurred.
- * @param  file: pointer to the source file name
- * @param  line: assert_param error line source number
- * @retval None
- */
+* @brief  Reports the name of the source file and the source line number
+*         where the assert_param error has occurred.
+* @param  file: pointer to the source file name
+* @param  line: assert_param error line source number
+* @retval None
+*/
 void assert_failed(uint8_t *file, uint32_t line)
 {
 	/* USER CODE BEGIN 6 */
 	/* User can add his own implementation to report the file name and line number,
-     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+	tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 	/* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
