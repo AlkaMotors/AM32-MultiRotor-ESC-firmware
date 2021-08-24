5df58734850536645ca5c97cf9750c4261bc661a
cruwaller <cruwaller@gmail.com>
2021-08-24 17:05:25 +0300

    Makefiles updated to allow build all targets without additional file modifications

--- f051makefile.mk
+++ f051makefile.mk
@@ -1,23 +1,26 @@
-CC = $(ARM_SDK_PREFIX)gcc
-CP = $(ARM_SDK_PREFIX)objcopy
-MCU := -mcpu=cortex-m0 -mthumb
-LDSCRIPT := Mcu/f051/STM32F051K6TX_FLASH.ld
-LIBS := -lc -lm -lnosys
-LDFLAGS := -specs=nano.specs -T$(LDSCRIPT) $(LIBS) -Wl,--gc-sections -Wl,--print-memory-usage
-MAIN_SRC_DIR := Src
-SRC_DIR := Mcu/f051/Startup \
-	Src \
-    Mcu/f051/Src \
-	Mcu/f051/Drivers/STM32F0xx_HAL_Driver/Src
-SRC := $(foreach dir,$(SRC_DIR),$(wildcard $(dir)/*.[cs]))
-INCLUDES :=  \
-	-IInc \
-	-IMcu/f051/Inc \
-	-IMcu/f051/Drivers/STM32F0xx_HAL_Driver/Inc \
-	-IMcu/f051/Drivers/CMSIS/Include \
-	-IMcu/f051/Drivers/CMSIS/Device/ST/STM32F0xx/Include
-VALUES :=  \
-	-DUSE_MAKE \
+
+TARGETS_F051 := \
+	FD6288 MP6531 IFLIGHT TMOTOR55 TMOTOR45 HGLRC SISKIN \
+	DIATONE MAMBA_F40PRO MAMBA_F50PRO MAMBA_F60PRO \
+	WRAITH32 CRTEENSY_HILARIESC
+
+HAL_FOLDER_F051 := $(HAL_FOLDER)/f051
+
+MCU_F051 := -mcpu=cortex-m0 -mthumb
+LDSCRIPT_F051 := $(HAL_FOLDER_F051)/STM32F051K6TX_FLASH.ld
+
+SRC_DIR_F051 += \
+	$(HAL_FOLDER_F051)/Startup \
+	$(HAL_FOLDER_F051)/Src \
+	$(HAL_FOLDER_F051)/Drivers/STM32F0xx_HAL_Driver/Src
+
+CFLAGS_F051 := \
+	-I$(HAL_FOLDER_F051)/Inc \
+	-I$(HAL_FOLDER_F051)/Drivers/STM32F0xx_HAL_Driver/Inc \
+	-I$(HAL_FOLDER_F051)/Drivers/CMSIS/Include \
+	-I$(HAL_FOLDER_F051)/Drivers/CMSIS/Device/ST/STM32F0xx/Include
+
+CFLAGS_F051 += \
 	-DHSE_VALUE=8000000 \
 	-DSTM32F051x8 \
 	-DHSE_STARTUP_TIMEOUT=100 \
@@ -30,63 +33,5 @@ VALUES :=  \
 	-DHSI_VALUE=8000000 \
 	-DUSE_FULL_LL_DRIVER \
 	-DPREFETCH_ENABLE=1
-CFLAGS = $(MCU) $(VALUES) $(INCLUDES) -O2 -Wall -fdata-sections -ffunction-sections
-CFLAGS += -D$(TARGET)
-CFLAGS += -MMD -MP -MF $(@:%.bin=%.d)
 
-TARGETS := FD6288 MP6531 IFLIGHT TMOTOR55 TMOTOR45 HGLRC SISKIN DIATONE MAMBA_F40PRO MAMBA_F50PRO MAMBA_F60PRO WRAITH32 CRTEENSY_HILARIESC
-
-# Working directories
-ROOT := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
-
-IDENTIFIER    := AM32
-VERSION_MAJOR := $(shell grep " VERSION_MAJOR" Src/main.c | awk '{print $$3}' )
-VERSION_MINOR := $(shell grep " VERSION_MINOR" Src/main.c | awk '{print $$3}' )
-
-FIRMWARE_VERSION := $(VERSION_MAJOR).$(VERSION_MINOR)
-
-TARGET_BASENAME = $(BIN_DIR)/$(IDENTIFIER)_$(TARGET)_$(FIRMWARE_VERSION)
-
-# Build tools, so we all share the same versions
-# import macros common to all supported build systems
-include $(ROOT)/make/system-id.mk
-
-# configure some directories that are relative to wherever ROOT_DIR is located
-BIN_DIR := $(ROOT)/obj
-
-TOOLS_DIR ?= $(ROOT)/tools
-DL_DIR := $(ROOT)/downloads
-
-.PHONY : clean all
-all : $(TARGETS)
-
-clean :
-	rm -rf $(BIN_DIR)/*
-
-$(TARGETS) :
-	$(MAKE) TARGET=$@ binary
-
-binary : $(TARGET_BASENAME).bin
-
-$(TARGET_BASENAME).bin : $(SRC)
-	mkdir -p $(dir $@)
-	$(CC) $(CFLAGS) $(LDFLAGS) -o $(TARGET_BASENAME).elf $(SRC)
-	$(CP) -O binary $(TARGET_BASENAME).elf $(TARGET_BASENAME).bin
-	$(CP) $(TARGET_BASENAME).elf -O ihex  $(TARGET_BASENAME).hex
-
-# mkdirs
-$(DL_DIR):
-	mkdir -p $@
-
-$(TOOLS_DIR):
-	mkdir -p $@
-
-# include the tools makefile
-include $(ROOT)/make/tools.mk
-
-
-
-
-
-
-
+SRC_F051 := $(foreach dir,$(SRC_DIR_F051),$(wildcard $(dir)/*.[cs]))
