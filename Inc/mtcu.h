4cdfb2261f5d19235bcd880fbbb70e5141bb08fd
Heclo <knudski@gmail.com>
2024-02-11 18:27:27 +0100

    Add files via upload

---
+++ Inc/mtcu.h
@@ -0,0 +1,16 @@
+/*
+ * mtcu.h
+ *
+ *  Created on: Feb 11, 2024
+ *      Author: heclo
+ */
+
+#ifndef MTCU_H_
+#define MTCU_H_
+
+#include "main.h"
+
+
+void setChannelsMTCU();
+
+#endif /* MTCU_H_ */
